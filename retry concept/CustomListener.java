22de88d39e04da8212e0d0779498dafca8f8d7d2
Malathi92 <103888118+Malathi92@users.noreply.github.com>
2022-06-28 09:38:18 +0400

    Add files via upload

---
+++ retry concept/CustomListener.java
@@ -0,0 +1,37 @@
+package Retry;
+
+
+	
+	
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import org.testng.IAnnotationTransformer;
+import org.testng.IRetryAnalyzer;
+	import org.testng.ITestResult;
+import org.testng.annotations.ITestAnnotation;
+
+	public class CustomListener implements IRetryAnalyzer,IAnnotationTransformer{
+
+		int count=0;
+		public boolean retry(ITestResult result) {
+			// TODO Auto-generated method stub
+			if(count<2)
+			{
+				count++;
+				return true;
+			}
+			return false;
+		}
+		public void transform(
+			      ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) 
+		{
+			annotation.setRetryAnalyzer(CustomListener.class);
+			
+			    // not implemented
+			  }
+
+	}
+
+
+
